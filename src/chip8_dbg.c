56dfa696536601ab343c29396896612a6dc725ce
Patrick Cardoso <thetickbigboss@protonmail.com>
2021-10-21 21:59:14 -0300

    Added a simple disassembler. It's called in the cpu_debugger() function

--- src/chip8_dbg.c
+++ src/chip8_dbg.c
@@ -5,6 +5,133 @@
 #endif
 #include "chip8.h"
 
+void disassembler(uint16_t opcode) {
+	switch(opcode & 0xF000) {
+		case 0x0000:
+			printw("SYS %03X", opcode & 0x0FFF);
+			break;
+		case 0x00E0:
+			printw("CLS");
+			break;
+		case 0x00EE:
+			printw("RET");
+			break;
+		case 0x1000:
+			printw("JP %03X", opcode & 0x0FFF);
+			break;
+		case 0x2000:
+			printw("CALL %03X", opcode & 0x0FFF);
+			break;
+		case 0x3000:
+			printw("SE V%X, %02X", (opcode & 0x0F00) >> 8, opcode & 0x00FF);
+			break;
+		case 0x4000:
+			printw("SNE V%X, %02X", (opcode & 0x0F00) >> 8, opcode & 0x00FF);
+			break;
+		case 0x5000:
+			printw("SE V%X, V%X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+			break;
+		case 0x6000:
+			printw("LD V%X, %02X", (opcode & 0x0F00) >> 8, opcode & 0x00FF);
+			break;
+		case 0x7000:
+			printw("ADD V%X, %02X", (opcode & 0x0F00) >> 8, opcode & 0x00FF);
+			break;
+		case 0x8000:
+			switch(opcode & 0x000F) {
+				case 0x0000:
+					printw("LD V%X, V%X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+					break;
+				case 0x0001:
+					printw("OR V%X, V%X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+					break;
+				case 0x0002:
+					printw("AND V%X, V%X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+					break;
+				case 0x0003:
+					printw("XOR V%X, V%X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+					break;
+				case 0x0004:
+					printw("ADD V%X, V%X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+					break;
+				case 0x0005:
+					printw("SUB V%X, V%X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+					break;
+				case 0x0006:
+					printw("SHR V%X {, V%X}", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+					break;
+				case 0x0007:
+					printw("SUBN V%X, V%X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+					break;
+				case 0x000E:
+					printw("SHL V%X, {, V%X}", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+					break;
+				default:
+					printw("Unknown opcode 0x%04X", opcode);
+			}
+			break;
+		case 0x9000:
+			printw("SNE V%X, V%X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4);
+			break;
+		case 0xA000:
+			printw("LD I, %03X", opcode & 0x0FFF);
+			break;
+		case 0xB000:
+			printw("JP V0, %03X", opcode & 0x0FFF);
+			break;
+		case 0xC000:
+			printw("RND V%X, %02X", (opcode & 0x0F00) >> 8, opcode & 0x0FF);
+			break;
+		case 0xD000:
+			printw("DRW V%X, V%X, %X", (opcode & 0x0F00) >> 8, (opcode & 0x00F0) >> 4, opcode & 0x000F);
+			break;
+		case 0xE000:
+			switch(opcode & 0x00FF) {
+				case 0x009E:
+					printw("SKP V%X", (opcode & 0x0F00) >> 8);
+					break;
+				case 0x00A1:
+					printw("SKNP V%X", (opcode & 0x0F00) >> 8);
+					break;
+				default:
+					printw("Unknown opcode 0x%04X", opcode);
+			}
+			break;
+		case 0xF000:
+			switch(opcode & 0x00FF) {
+				case 0x0007:
+					printw("LD V%X, DT", (opcode & 0x0F00) >> 8);
+					break;
+				case 0x0015:
+					printw("LD DT, V%X", (opcode & 0x0F00) >> 8);
+					break;
+				case 0x0018:
+					printw("LD ST V%X", (opcode & 0x0F00) >> 8);
+					break;
+				case 0x001E:
+					printw("ADD I, V%X", (opcode & 0x0F00) >> 8);
+					break;
+				case 0x0029:
+					printw("LD F, V%X", (opcode & 0x0F00) >> 8);
+					break;
+				case 0x0033:
+					printw("LD B, V%X", (opcode & 0x0F00) >> 8);
+					break;
+				case 0x0055:
+					printw("LD [I], V%X", (opcode & 0x0F00) >> 8);
+					break;
+				case 0x0065:
+					printw("LD V%X, [I]", (opcode & 0x0F00) >> 8);
+					break;
+				default:
+					printw("Unknown opcode 0x%04X", opcode);
+			}
+			break;
+		default:
+			printw("Unknown opcode 0x%04X", opcode);
+	}
+}
+
 void mem_debugger(size_t n) {
 	size_t i;
 
@@ -16,8 +143,6 @@ void mem_debugger(size_t n) {
 
 /* Views processor registers */
 void cpu_debugger(void) {
-	uint16_t next_op = memory[cpu.pc] << 8 | memory[cpu.pc + 1];
-
 	attron(A_BOLD);
 	addstr("Registers\n");
 	attroff(A_BOLD);
@@ -31,7 +156,10 @@ void cpu_debugger(void) {
 
 	printw("PC: 0x%02X\tsp: 0x%X\n", cpu.pc, cpu.sp);
 	printw("I : 0x%02X\n", cpu.I);
-	printw("op: 0x%02X\tNext op.: 0x%02X\n", cpu.opcode, next_op);
+
+	printw("op: ");
+	disassembler(cpu.opcode);
+	printw(" (0x%02X)", cpu.opcode);
 
 	refresh();
 	erase();
